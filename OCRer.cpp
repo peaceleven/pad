6de3e20dc9cd04726e2d0fe3fddea46a374d522c
Liu Jialong <jialong08@gmail.com>
2012-10-21 02:20:27 +0800

    limit tesseract to recognize common chars only

--- OCRer.cpp
+++ OCRer.cpp
@@ -3,9 +3,15 @@
 namespace pad {
 
 OCRer::OCRer() {
-	const char* tessdata_path = "/usr/local/share/tessdata/";
+	char tessdata_path[] = "/usr/local/share/tessdata/";
+
+	int n_configs = 1;
+	char tess_config0[] = "common";
+	char *tess_configs[n_configs];
+	tess_configs[0] = tess_config0;
+
 	_tess = new tesseract::TessBaseAPI();
-	_tess->Init(tessdata_path, "eng");
+	_tess->Init(tessdata_path, "eng", tesseract::OEM_TESSERACT_ONLY, tess_configs, n_configs, NULL, NULL, false);
 	setenv("TESSDATA_PREFIX", tessdata_path, 1);
 }
 OCRer::~OCRer() {
@@ -15,7 +21,7 @@ OCRer::~OCRer() {
 
 void OCRer::process_image(const unsigned char *img_data, int bytes_per_pixel, int bytes_per_line, int left, int top, int width, int height) {
 	char *s = _tess->TesseractRect(img_data, bytes_per_pixel, bytes_per_line, left, top, width, height);
-	cout << "OCR text:" << s;
+	cout << s;
 	free(s);
 }
 
